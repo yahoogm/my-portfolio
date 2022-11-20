0ddef154afb5b8afdace20438ed34fe3c5c16d77
yahoogm <yaho.garfield@gmail.com>
2022-11-20 12:59:01 +0700

    Add styling animation

--- my-portfolio/components/Tech.jsx
+++ my-portfolio/components/Tech.jsx
@@ -1,7 +1,7 @@
 const Tech = ({ tech }) => {
   return (
-    <div className="bg-low-priority hover:bg-primary w-full grid justify-items-center rounded-md shadow-md md:p-4 lg:p-8">
-      <div className="text-white">{tech.icon}</div>
+    <div className="bg-low-priority hover:bg-primary w-full grid justify-items-center rounded-md shadow-md p-2 md:p-4 lg:p-8 transition ease-in duration-300">
+      <div className="text-white text-7xl md:text-8xl">{tech.icon}</div>
       <div className="text-white font-semibold md:text-lg">{tech.title}</div>
     </div>
   );
